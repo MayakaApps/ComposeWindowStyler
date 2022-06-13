86120149b35a87a3c17026baa064e4c190b8d5b2
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-13 02:41:08 +0200

    Wait for the window to become visible first
    If the window isn't shown yet, and we try to apply Default, Solid, Aero, or Acrylic, the effect will be applied to the title bar background leaving the caption with awkward background box.
    Unfortunately, even with this method, mica in light mode has this background box.

--- window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/WindowStyle.kt
+++ window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/WindowStyle.kt
@@ -10,6 +10,7 @@ import com.mayakapps.compose.windowstyler.WindowManager.setMicaEffectEnabled
 import com.mayakapps.compose.windowstyler.WindowManager.setSystemBackdrop
 import com.mayakapps.compose.windowstyler.jna.enums.AccentFlag
 import com.mayakapps.compose.windowstyler.jna.enums.AccentState
+import javax.swing.SwingUtilities
 import javax.swing.UIManager
 
 @Composable
@@ -33,58 +34,66 @@ fun FrameWindowScope.WindowStyle(
     }
 
     LaunchedEffect(backdropType) {
-        // Set [ACCENT_DISABLED] as [ACCENT_POLICY] to apply styles properly.
-        setAccentPolicy(hwnd, AccentState.ACCENT_DISABLED)
+        // This is done to make sure that the window has become visible
+        // If the window isn't shown yet, and we try to apply Default, Solid, Aero,
+        // or Acrylic, the effect will be applied to the title bar background
+        // leaving the caption with awkward background box.
+        // Unfortunately, even with this method, mica in light mode has this
+        // background box.
+        SwingUtilities.invokeLater {
+            // Set [ACCENT_DISABLED] as [ACCENT_POLICY] to apply styles properly.
+            setAccentPolicy(hwnd, AccentState.ACCENT_DISABLED)
 
-        if (backdropType is WindowBackdrop.Transparent) return@LaunchedEffect
+            if (backdropType is WindowBackdrop.Transparent) return@invokeLater
 
-        // Only on later Windows 11 versions and if effect is WindowEffect.mica,
-        // WindowEffect.acrylic or WindowEffect.tabbed, otherwise fallback to old
-        // approach.
-        if (
-            windowsBuild >= 22523 &&
-            (backdropType is WindowBackdrop.Acrylic || backdropType is WindowBackdrop.Mica || backdropType is WindowBackdrop.Tabbed)
-        ) {
-            createSheetOfGlassEffect(hwnd)
-            setSystemBackdrop(hwnd, backdropType.toDwmSystemBackdrop())
-        } else {
-            if (backdropType is WindowBackdrop.Mica) {
-                // Check for Windows 11.
-                if (windowsBuild >= 22000) {
-                    createSheetOfGlassEffect(hwnd)
-                    setMicaEffectEnabled(hwnd, true)
-                }
+            // Only on later Windows 11 versions and if effect is WindowEffect.mica,
+            // WindowEffect.acrylic or WindowEffect.tabbed, otherwise fallback to old
+            // approach.
+            if (
+                windowsBuild >= 22523 &&
+                (backdropType is WindowBackdrop.Acrylic || backdropType is WindowBackdrop.Mica || backdropType is WindowBackdrop.Tabbed)
+            ) {
+                createSheetOfGlassEffect(hwnd)
+                setSystemBackdrop(hwnd, backdropType.toDwmSystemBackdrop())
             } else {
-                // Restore original window style & [DwmExtendFrameIntoClientArea] margin
-                // if the last set effect was [WindowEffect.mica], since it sets
-                // negative margins to the window.
-                if (
-                    (windowsBuild >= 22000 && lastEffect is WindowBackdrop.Mica) ||
-                    (windowsBuild >= 22523 && (lastEffect is WindowBackdrop.Acrylic || lastEffect is WindowBackdrop.Tabbed))
-                ) {
-                    restoreWindowFrame(hwnd)
-                    setMicaEffectEnabled(hwnd, false)
+                if (backdropType is WindowBackdrop.Mica) {
+                    // Check for Windows 11.
+                    if (windowsBuild >= 22000) {
+                        createSheetOfGlassEffect(hwnd)
+                        setMicaEffectEnabled(hwnd, true)
+                    }
+                } else {
+                    // Restore original window style & [DwmExtendFrameIntoClientArea] margin
+                    // if the last set effect was [WindowEffect.mica], since it sets
+                    // negative margins to the window.
+                    if (
+                        (windowsBuild >= 22000 && lastEffect is WindowBackdrop.Mica) ||
+                        (windowsBuild >= 22523 && (lastEffect is WindowBackdrop.Acrylic || lastEffect is WindowBackdrop.Tabbed))
+                    ) {
+                        restoreWindowFrame(hwnd)
+                        setMicaEffectEnabled(hwnd, false)
+                    }
+
+                    setAccentPolicy(
+                        hwnd = hwnd,
+                        accentState = when (backdropType) {
+                            is WindowBackdrop.Default, is WindowBackdrop.Solid -> AccentState.ACCENT_ENABLE_GRADIENT
+                            is WindowBackdrop.Aero -> AccentState.ACCENT_ENABLE_BLURBEHIND
+                            is WindowBackdrop.Acrylic -> AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND
+                            else -> throw IllegalStateException()
+                        },
+                        accentFlags = setOf(AccentFlag.DRAW_ALL_BORDERS),
+                        color = when (backdropType) {
+                            is WindowBackdrop.Default -> UIManager.getColor("Panel.background").rgb
+                            is WindowBackdrop.Solid -> backdropType.color
+                            is WindowBackdrop.Acrylic -> backdropType.color
+                            else -> 0x7FFFFFFF
+                        },
+                    )
                 }
-
-                setAccentPolicy(
-                    hwnd = hwnd,
-                    accentState = when (backdropType) {
-                        is WindowBackdrop.Default, is WindowBackdrop.Solid -> AccentState.ACCENT_ENABLE_GRADIENT
-                        is WindowBackdrop.Aero -> AccentState.ACCENT_ENABLE_BLURBEHIND
-                        is WindowBackdrop.Acrylic -> AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND
-                        else -> throw IllegalStateException()
-                    },
-                    accentFlags = setOf(AccentFlag.DRAW_ALL_BORDERS),
-                    color = when (backdropType) {
-                        is WindowBackdrop.Default -> UIManager.getColor("Panel.background").rgb
-                        is WindowBackdrop.Solid -> backdropType.color
-                        is WindowBackdrop.Acrylic -> backdropType.color
-                        else -> 0x7FFFFFFF
-                    },
-                )
             }
-        }
 
-        lastEffect = backdropType
+            lastEffect = backdropType
+        }
     }
 }
