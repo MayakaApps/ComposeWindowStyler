08ff7d00597b24087b726ad796cf340392607622
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-13 02:26:27 +0200

    Update demo to be more customizable

--- window-styler-demo/src/jvmMain/kotlin/Main.kt
+++ window-styler-demo/src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,10 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -16,27 +14,52 @@ import com.mayakapps.compose.windowstyler.WindowStyle
 
 @Composable
 @Preview
-fun App(isDark: Boolean = isSystemInDarkTheme()) {
-    MaterialTheme(colors = if (isDark) darkColors() else lightColors()) {
-        Column {
+fun App(
+    isDarkTheme: Boolean,
+    onThemeChange: (Boolean) -> Unit,
+    backdropType: WindowBackdrop,
+    onBackdropChange: (WindowBackdrop) -> Unit,
+) {
+    MaterialTheme(colors = if (isDarkTheme) darkColors() else lightColors()) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            RadioGroup("Theme", themeOptions, isDarkTheme, onThemeChange)
             Spacer(Modifier.height(50.dp))
-            Row {
-                Spacer(Modifier.width(200.dp))
-
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    elevation = 4.dp,
-                    shape = RoundedCornerShape(topStart = 12.dp),
-                ) {}
-            }
+            RadioGroup("Backdrop Type", backdropOptions, backdropType, onBackdropChange)
         }
     }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        WindowStyle(isDarkTheme = isSystemInDarkTheme(), backdropType = WindowBackdrop.Mica)
+        var isDarkTheme by remember { mutableStateOf(false) }
+        var backdropType by remember { mutableStateOf<WindowBackdrop>(WindowBackdrop.Default) }
 
-        App()
+        WindowStyle(isDarkTheme = isDarkTheme, backdropType = backdropType)
+
+        App(
+            isDarkTheme = isDarkTheme,
+            onThemeChange = { isDarkTheme = it },
+            backdropType = backdropType,
+            onBackdropChange = { backdropType = it },
+        )
     }
 }
+
+val themeOptions = listOf(false to "Light", true to "Dark")
+val backdropOptions = listOf(
+    WindowBackdrop.Default to "Default",
+    WindowBackdrop.Solid(0xFF0000) to "Solid Red",
+    WindowBackdrop.Solid(0x0000FF) to "Solid Blue",
+    WindowBackdrop.Transparent to "Transparent",
+    WindowBackdrop.Aero to "Aero",
+    WindowBackdrop.Acrylic(0xFF0000) to "Acrylic Red",
+    WindowBackdrop.Acrylic(0x0000FF) to "Acrylic Blue",
+    WindowBackdrop.Mica to "Mica",
+    WindowBackdrop.Tabbed to "Tabbed",
+)
