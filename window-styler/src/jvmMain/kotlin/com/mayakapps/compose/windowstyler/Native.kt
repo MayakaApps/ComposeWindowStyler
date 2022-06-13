4f0a04d5caf98a9b8726efec9ba4f939e657987f
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-13 17:25:04 +0200

    Rename WindowManager to Native and improve it

--- window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/Native.kt
+++ window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/Native.kt
@@ -13,18 +13,18 @@ import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinDef.BOOL
 import com.sun.jna.platform.win32.WinDef.HWND
 
-internal object WindowManager {
+internal object Native {
 
     fun createSheetOfGlassEffect(hwnd: HWND) =
         extendFrameIntoClientArea(hwnd, -1, -1, -1, -1)
 
-    fun restoreWindowFrame(hwnd: HWND) {
+    fun restoreWindowFrame(hwnd: HWND): Boolean {
         // At least one margin should be non-negative in order to show the DWM
         // window shadow created by handling [WM_NCCALCSIZE].
         //
         // Matching value with bitsdojo_window.
         // https://github.com/bitsdojo/bitsdojo_window/blob/adad0cd40be3d3e12df11d864f18a96a2d0fb4fb/bitsdojo_window_windows/windows/bitsdojo_window.cpp#L149
-        extendFrameIntoClientArea(hwnd, 0, 0, 1, 0)
+        return extendFrameIntoClientArea(hwnd, 0, 0, 1, 0)
     }
 
     private fun extendFrameIntoClientArea(
@@ -33,17 +33,18 @@ internal object WindowManager {
         rightWidth: Int = 0,
         topHeight: Int = 0,
         bottomHeight: Int = 0,
-    ) {
+    ): Boolean {
         val margins = Margins(leftWidth, rightWidth, topHeight, bottomHeight)
 
         val result = Dwm.DwmExtendFrameIntoClientArea(hwnd, margins)
         if (result != W32Errors.S_OK) println("DwmExtendFrameIntoClientArea failed with result $result")
 
         margins.dispose()
+        return result == W32Errors.S_OK
     }
 
 
-    fun setImmersiveDarkModeEnabled(hwnd: HWND, enabled: Boolean) {
+    fun setImmersiveDarkModeEnabled(hwnd: HWND, enabled: Boolean): Boolean {
         val result = Dwm.DwmSetWindowAttribute(
             hwnd,
             if (windowsBuild >= 18985) DwmWindowAttribute.DWMWA_USE_IMMERSIVE_DARK_MODE
@@ -53,9 +54,10 @@ internal object WindowManager {
         )
 
         if (result != W32Errors.S_OK) println("DwmSetWindowAttribute(DWMWA_USE_IMMERSIVE_DARK_MODE) failed with result $result")
+        return result == W32Errors.S_OK
     }
 
-    fun setSystemBackdrop(hwnd: HWND, systemBackdrop: DwmSystemBackdrop) {
+    fun setSystemBackdrop(hwnd: HWND, systemBackdrop: DwmSystemBackdrop): Boolean {
         val result = Dwm.DwmSetWindowAttribute(
             hwnd,
             DwmWindowAttribute.DWMWA_SYSTEMBACKDROP_TYPE,
@@ -64,9 +66,10 @@ internal object WindowManager {
         )
 
         if (result != W32Errors.S_OK) println("DwmSetWindowAttribute(DWMWA_MICA_EFFECT) failed with result $result")
+        return result == W32Errors.S_OK
     }
 
-    fun setMicaEffectEnabled(hwnd: HWND, enabled: Boolean) {
+    fun setMicaEffectEnabled(hwnd: HWND, enabled: Boolean): Boolean {
         val result = Dwm.DwmSetWindowAttribute(
             hwnd,
             DwmWindowAttribute.DWMWA_MICA_EFFECT,
@@ -75,6 +78,7 @@ internal object WindowManager {
         )
 
         if (result != W32Errors.S_OK) println("DwmSetWindowAttribute(DWMWA_MICA_EFFECT) failed with result $result")
+        return result == W32Errors.S_OK
     }
 
 
@@ -84,18 +88,20 @@ internal object WindowManager {
         accentFlags: Set<AccentFlag> = emptySet(),
         color: Int = 0,
         animationId: Int = 0,
-    ) {
-        Native.setLastError(0)
-
-        val isSuccess = User32.SetWindowCompositionAttribute(
-            hwnd,
-            WindowCompositionAttributeData(
-                WindowCompositionAttribute.WCA_ACCENT_POLICY,
-                AccentPolicy(accentState, accentFlags, color, animationId),
-            ),
+    ): Boolean {
+        val data = WindowCompositionAttributeData(
+            WindowCompositionAttribute.WCA_ACCENT_POLICY,
+            AccentPolicy(accentState, accentFlags, color, animationId),
         )
 
+        Native.setLastError(0)
+
+        val isSuccess = User32.SetWindowCompositionAttribute(hwnd, data)
+
         if (!isSuccess) println("SetWindowCompositionAttribute(WCA_ACCENT_POLICY) failed with last error ${Native.getLastError()}")
+
+        data.dispose()
+        return isSuccess
     }
 
     private const val INT_SIZE = 4
