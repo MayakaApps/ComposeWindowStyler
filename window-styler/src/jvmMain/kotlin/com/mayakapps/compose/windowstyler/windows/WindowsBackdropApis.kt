6395033731eaab5d94192606cbe8e2cab9427f72
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-16 07:51:34 +0200

    Separate backdrop api tracking from WindowsWindowManager

---
+++ window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/windows/WindowsBackdropApis.kt
@@ -0,0 +1,76 @@
+package com.mayakapps.compose.windowstyler.windows
+
+import com.mayakapps.compose.windowstyler.windows.jna.Dwm
+import com.mayakapps.compose.windowstyler.windows.jna.User32
+import com.mayakapps.compose.windowstyler.windows.jna.enums.AccentFlag
+import com.mayakapps.compose.windowstyler.windows.jna.enums.AccentState
+import com.mayakapps.compose.windowstyler.windows.jna.enums.DwmSystemBackdrop
+import com.mayakapps.compose.windowstyler.windows.jna.enums.DwmWindowAttribute
+import com.sun.jna.platform.win32.WinDef
+
+internal class WindowsBackdropApis(private val hwnd: WinDef.HWND) {
+    private var isSystemBackdropSet = false
+    private var isMicaEnabled = false
+    private var isAccentPolicySet = false
+    private var isSheetOfGlassApplied = false
+
+    fun setSystemBackdrop(systemBackdrop: DwmSystemBackdrop) {
+        createSheetOfGlassEffect()
+        if (Dwm.setSystemBackdrop(hwnd, systemBackdrop)) {
+            isSystemBackdropSet = systemBackdrop == DwmSystemBackdrop.DWMSBT_DISABLE
+            if (isSystemBackdropSet) resetAccentPolicy()
+        }
+    }
+
+    fun setMicaEffectEnabled(enabled: Boolean) {
+        createSheetOfGlassEffect()
+        if (Dwm.setWindowAttribute(hwnd, DwmWindowAttribute.DWMWA_MICA_EFFECT, enabled)) {
+            isMicaEnabled = enabled
+            if (isMicaEnabled) resetAccentPolicy()
+        }
+    }
+
+    fun setAccentPolicy(
+        accentState: AccentState = AccentState.ACCENT_DISABLED,
+        accentFlags: Set<AccentFlag> = emptySet(),
+        color: Int = 0,
+        animationId: Int = 0,
+    ) {
+        if (User32.setAccentPolicy(hwnd, accentState, accentFlags, color, animationId)) {
+            isAccentPolicySet = accentState != AccentState.ACCENT_DISABLED
+            if (isAccentPolicySet) {
+                resetSystemBackdrop()
+                resetMicaEffectEnabled()
+                resetWindowFrame()
+            }
+        }
+    }
+
+    fun createSheetOfGlassEffect() {
+        if (!isSheetOfGlassApplied && Dwm.extendFrameIntoClientArea(hwnd, -1)) isSheetOfGlassApplied = true
+    }
+
+
+    fun resetSystemBackdrop() {
+        if (isSystemBackdropSet) setSystemBackdrop(DwmSystemBackdrop.DWMSBT_DISABLE)
+    }
+
+    fun resetMicaEffectEnabled() {
+        if (isMicaEnabled) setMicaEffectEnabled(false)
+    }
+
+    fun resetAccentPolicy() {
+        if (isAccentPolicySet) setAccentPolicy(AccentState.ACCENT_DISABLED)
+    }
+
+    fun resetWindowFrame() {
+        // At least one margin should be non-negative in order to show the DWM
+        // window shadow created by handling [WM_NCCALCSIZE].
+        //
+        // Matching value with bitsdojo_window.
+        // https://github.com/bitsdojo/bitsdojo_window/blob/adad0cd40be3d3e12df11d864f18a96a2d0fb4fb/bitsdojo_window_windows/windows/bitsdojo_window.cpp#L149
+        if (isSheetOfGlassApplied && Dwm.extendFrameIntoClientArea(hwnd, 0, 0, 1, 0)) {
+            isSheetOfGlassApplied = false
+        }
+    }
+}
--- window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/windows/WindowsWindowManager.kt
+++ window-styler/src/jvmMain/kotlin/com/mayakapps/compose/windowstyler/windows/WindowsWindowManager.kt
@@ -4,10 +4,7 @@ import androidx.compose.ui.awt.ComposeWindow
 import androidx.compose.ui.graphics.Color
 import com.mayakapps.compose.windowstyler.*
 import com.mayakapps.compose.windowstyler.windows.jna.Dwm
-import com.mayakapps.compose.windowstyler.windows.jna.User32
 import com.mayakapps.compose.windowstyler.windows.jna.enums.AccentFlag
-import com.mayakapps.compose.windowstyler.windows.jna.enums.AccentState
-import com.mayakapps.compose.windowstyler.windows.jna.enums.DwmSystemBackdrop
 import com.mayakapps.compose.windowstyler.windows.jna.enums.DwmWindowAttribute
 import com.sun.jna.platform.win32.WinDef.HWND
 import java.awt.Window
@@ -23,6 +20,8 @@ class WindowsWindowManager(
 
     private val hwnd: HWND = window.hwnd
     private val isUndecorated = window.isUndecorated
+
+    private val backdropApis = WindowsBackdropApis(hwnd)
 
     override var isDarkTheme: Boolean = isDarkTheme
         set(value) {
@@ -73,10 +72,6 @@ class WindowsWindowManager(
     }
 
     private var wasAero = false
-    private var isSystemBackdropSet = false
-    private var isMicaEnabled = false
-    private var isAccentPolicySet = false
-    private var isSheetOfGlassApplied = false
 
     private fun updateTheme() {
         val attribute =
@@ -113,12 +108,12 @@ class WindowsWindowManager(
                 windowsBuild >= 22523 &&
                 (backdropType is WindowBackdrop.Acrylic || backdropType is WindowBackdrop.Mica || backdropType is WindowBackdrop.Tabbed)
             ) {
-                setSystemBackdrop(backdropType.toDwmSystemBackdrop())
+                backdropApis.setSystemBackdrop(backdropType.toDwmSystemBackdrop())
             } else {
                 if (backdropType is WindowBackdrop.Mica) {
                     // Check for Windows 11.
                     if (windowsBuild >= 22000) {
-                        setMicaEffectEnabled(true)
+                        backdropApis.setMicaEffectEnabled(true)
                     }
                 } else {
                     val color = when (val backdropType = backdropType) {
@@ -132,16 +127,16 @@ class WindowsWindowManager(
 
                     // wasAero: This is required as sometimes the window gets stuck at aero
                     // Transparent: In many cases, if this is not done, red opaque background is shown
-                    if (wasAero || backdropType is WindowBackdrop.Transparent) resetAccentPolicy()
+                    if (wasAero || backdropType is WindowBackdrop.Transparent) backdropApis.resetAccentPolicy()
 
                     // Another red opaque background case :'(
                     // Resetting these values needs to be done before applying transparency
                     if (backdropType is WindowBackdrop.Transparent) {
-                        resetMicaEffectEnabled()
-                        resetSystemBackdrop()
+                        backdropApis.resetMicaEffectEnabled()
+                        backdropApis.resetSystemBackdrop()
                     }
 
-                    setAccentPolicy(
+                    backdropApis.setAccentPolicy(
                         accentState = backdropType.toAccentState(),
                         accentFlags = setOf(AccentFlag.DRAW_ALL_BORDERS),
                         color = color,
@@ -150,68 +145,6 @@ class WindowsWindowManager(
             }
         }
     }
-
-
-    private fun setSystemBackdrop(systemBackdrop: DwmSystemBackdrop) {
-        createSheetOfGlassEffect()
-        if (Dwm.setSystemBackdrop(hwnd, systemBackdrop)) {
-            isSystemBackdropSet = systemBackdrop == DwmSystemBackdrop.DWMSBT_DISABLE
-            if (isSystemBackdropSet) resetAccentPolicy()
-        }
-    }
-
-    private fun setMicaEffectEnabled(enabled: Boolean) {
-        createSheetOfGlassEffect()
-        if (Dwm.setWindowAttribute(hwnd, DwmWindowAttribute.DWMWA_MICA_EFFECT, enabled)) {
-            isMicaEnabled = enabled
-            if (isMicaEnabled) resetAccentPolicy()
-        }
-    }
-
-    private fun setAccentPolicy(
-        accentState: AccentState = AccentState.ACCENT_DISABLED,
-        accentFlags: Set<AccentFlag> = emptySet(),
-        color: Int = 0,
-        animationId: Int = 0,
-    ) {
-        if (User32.setAccentPolicy(hwnd, accentState, accentFlags, color, animationId)) {
-            isAccentPolicySet = accentState != AccentState.ACCENT_DISABLED
-            if (isAccentPolicySet) {
-                resetSystemBackdrop()
-                resetMicaEffectEnabled()
-                resetWindowFrame()
-            }
-        }
-    }
-
-    private fun createSheetOfGlassEffect() {
-        if (!isSheetOfGlassApplied && Dwm.extendFrameIntoClientArea(hwnd, -1)) isSheetOfGlassApplied = true
-    }
-
-
-    private fun resetSystemBackdrop() {
-        if (isSystemBackdropSet) setSystemBackdrop(DwmSystemBackdrop.DWMSBT_DISABLE)
-    }
-
-    private fun resetMicaEffectEnabled() {
-        if (isMicaEnabled) setMicaEffectEnabled(false)
-    }
-
-    private fun resetAccentPolicy() {
-        if (isAccentPolicySet) setAccentPolicy(AccentState.ACCENT_DISABLED)
-    }
-
-    private fun resetWindowFrame() {
-        // At least one margin should be non-negative in order to show the DWM
-        // window shadow created by handling [WM_NCCALCSIZE].
-        //
-        // Matching value with bitsdojo_window.
-        // https://github.com/bitsdojo/bitsdojo_window/blob/adad0cd40be3d3e12df11d864f18a96a2d0fb4fb/bitsdojo_window_windows/windows/bitsdojo_window.cpp#L149
-        if (isSheetOfGlassApplied && Dwm.extendFrameIntoClientArea(hwnd, 0, 0, 1, 0)) {
-            isSheetOfGlassApplied = false
-        }
-    }
-
 
     private fun WindowBackdrop.fallbackIfUnsupported(): WindowBackdrop {
         if (windowsBuild >= supportedSince) return this
